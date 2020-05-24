1b137db01bbe0ce0242515a5adbda65f265cabb5
Guilherme Rangel <gui.vg16@hotmail.com>
2020-05-24 12:36:57 -0300

    settup initial

---
+++ MarvelApiChallenge/Presenter/Presenter.swift
@@ -0,0 +1,14 @@
+//
+//  Presenter.swift
+//  MarvelApiChallenge
+//
+//  Created by Guilherme Rangel on 24/05/20.
+//  Copyright © 2020 Guilherme Rangel. All rights reserved.
+//
+
+import Foundation
+
+class Presenter {
+    
+
+}
--- MarvelApiChallenge/View/ViewController.swift
+++ MarvelApiChallenge/View/ViewController.swift
@@ -2,17 +2,17 @@
 //  ViewController.swift
 //  MarvelApiChallenge
 //
-//  Created by Guilherme Rangel on 23/05/20.
+//  Created by Guilherme Rangel on 24/05/20.
 //  Copyright © 2020 Guilherme Rangel. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController  {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+     
     }
 
 
